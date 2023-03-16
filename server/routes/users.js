ac24abab369bd8cb37b30bc324c7400fc5fa8275
Jonathan Phair <jmphair@gmail.com>
2023-03-16 14:22:15 -0400

    add: initial queries

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,11 +1,15 @@
+// routes/users.js
 const express = require('express');
 const router = express.Router();
+const users = require('../db/queries/users');
 
-const users = ['Bob', 'Alex', 'Will', 'Tristan'];
 
-/* GET users listing */
+/* GET users listing. */
 router.get('/', (req, res) => {
-    res.json(users);
+  users.getAllUsers().then(data => {
+    console.log(data);
+    res.json({users: data});
+  })
 });
 
 module.exports = router;
