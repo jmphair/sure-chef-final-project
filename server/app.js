3bc3535eb8a63c70153b7c6110f7c3042ce39f2b
Giordano Temple <giordanotemple@gmail.com>
2023-03-17 17:52:19 -0400

    add:signup route structure

--- server/app.js
+++ server/app.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
 const app = express();
 app.use(cors());
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/users/login', usersRouter);
-app.use('/users/logout', usersRouter)
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/users/login", usersRouter);
+app.use("/users/logout", usersRouter);
+//app.use("/users/signup", userRouter);
 
 module.exports = app;
