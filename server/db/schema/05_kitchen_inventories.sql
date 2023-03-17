1e17b6bc16962e7019e8e53de4dbe67d503779c0
Giordano Temple <giordanotemple@gmail.com>
2023-03-17 10:00:20 -0400

    add: kitchen_inventories

---
+++ server/db/schema/05_kitchen_inventories.sql
@@ -0,0 +1,7 @@
+-- schema/05_create_kitchen_inventories.sql
+DROP TABLE IF EXISTS kitchen_inventories CASCADE;
+-- CREATE Grocery LISTS
+CREATE TABLE kitchen_inventories (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
