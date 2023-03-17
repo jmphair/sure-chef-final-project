2f93f533566328750e01c9506dcd55ce90068cac
dorreb <ashley_ure@hotmail.com>
2023-03-17 11:24:07 -0400

    fix: file name

--- server/db/schema/03_create_kitchen_inventories.sql
+++ server/db/schema/03_create_kitchen_inventories.sql
@@ -1,5 +1,6 @@
 -- schema/05_create_kitchen_inventories.sql
 DROP TABLE IF EXISTS kitchen_inventories CASCADE;
+
 -- CREATE Grocery LISTS
 CREATE TABLE kitchen_inventories (
   id SERIAL PRIMARY KEY,
