f7ee63bb5c8ad5449eb634fc59191049ea1e0e3b
dorreb <ashley_ure@hotmail.com>
2023-03-17 16:52:05 -0400

    fix: remove storage_location from recipes

--- server/db/schema/05_create_recipes.sql
+++ server/db/schema/05_create_recipes.sql
@@ -1,12 +1,12 @@
 -- schema/03_create_recipes.sql
 DROP TABLE IF EXISTS recipes CASCADE;
+
 -- CREATE RECIPES
 CREATE TABLE recipes (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   instructions TEXT NOT NULL,
   servings INTEGER,
-  storage_location VARCHAR(255),
   prep_time VARCHAR(255),
   cook_time VARCHAR(255),
   saved BOOLEAN DEFAULT false,
