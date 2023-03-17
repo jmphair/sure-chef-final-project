5d39973c9f8b9719c6f0d382c095590ca52eff8c
Jonathan Phair <jmphair@gmail.com>
2023-03-17 10:16:10 -0400

    add: seeds

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,8 +1,4 @@
 -- seeds/01_users.sql
 -- users seeds
-INSERT INTO users (id, username, email, password) VALUES (1, 'marty', 'rick.sandchez@gmail.com', 'picklerick');
-INSERT INTO users (id, username, email, password) VALUES (2, 'bar', 'lisa.simpson@gmail.com', 'ehhhhh');
-INSERT INTO users (id, username, email, password) VALUES (3, 'zelda', 'link@yahoo.com', 'hyrule');
-INSERT INTO users (id, username, email, password) VALUES (4, 'vamp', 'simon_bel123@mail.ca', 'dracula');
-INSERT INTO users (id, username, email, password) VALUES (5, 'anime', 'all_might@academia.jp', 'plusUltra');
-INSERT INTO users (id, username, email, password) VALUES (6, 'ihatewario', 'mario@mushroomkindom.jp', 'plumber79');
+INSERT INTO users (id, username, email, password) VALUES (1, 'zelda', 'link@yahoo.com', 'hyrule');
+INSERT INTO users (id, username, email, password) VALUES (2, 'ihatewario', 'mario@mushroomkindom.jp', 'plumber79');
---
+++ server/db/seeds/02_create_food_items.sql
@@ -0,0 +1,8 @@
+-- seeds/02_create_food_items.sql
+-- food_items seeds
+INSERT INTO food_items (id, kitchen_inventories_id, grocery_list_id, name, quantity, storage_location) VALUES (1, 1, 1, 'chicken breast', 4, 'freezer');
+INSERT INTO food_items (id, kitchen_inventories_id, grocery_list_id, name, quantity, storage_location) VALUES (2, 1, 1, 'potato', 10, 'fridge');
+INSERT INTO food_items (id, kitchen_inventories_id, grocery_list_id, name, quantity, storage_location) VALUES (3, 1, 1, 'tomato', 'pantry');
+INSERT INTO food_items (id, kitchen_inventories_id, grocery_list_id, name, quantity, storage_location) VALUES (4, 1, 1, 'mustard', 1, 'fridge');
+INSERT INTO food_items (id, kitchen_inventories_id, grocery_list_id, name, quantity, storage_location) VALUES (5, 1, 1, 'flour', 'pantry');
+INSERT INTO food_items (id, kitchen_inventories_id, grocery_list_id, name, quantity, storage_location) VALUES (6, 1, 1, 'butter', 'fridge');
