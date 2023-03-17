6db9a8ae07f9ae0212a48aa0dda9094fa856dd45
dorreb <ashley_ure@hotmail.com>
2023-03-17 16:40:36 -0400

    add: user queries

--- server/db/queries/food_items.js
+++ server/db/queries/food_items.js
@@ -3,12 +3,13 @@
 const db = require('../../configs/db.config');
 
 
-const getAllItemsForUserOnStorageLocation = (user_id, storage_location) => {
+const getAllItemsByStorageLocation = (user_id, storage_location) => {
   return db.query("SELECT * FROM food_items INNER JOIN kitchen_inventories ON food_items.kitchen_inventory_id = kitchen_inventories.id INNER JOIN users ON kitchen_inventories.user_id = users.id WHERE storage_location = $1 AND users.id = $2;", [storage_location, user_id]).then(data => {
     return data.rows;
-  });
+  })
+    .catch((error) => console.log(error));
 };
 
-module.exports = { getAllItemsForUserOnStorageLocation }
+module.exports = { getAllItemsByStorageLocation }
 
 
--- server/db/queries/users.js
+++ server/db/queries/users.js
@@ -3,15 +3,45 @@
 const db = require('../../configs/db.config');
 
 const getAllUsers = () => {
-	return db.query("SELECT * FROM users;").then(data => {
-		return data.rows;
-	})
-}
+  return db.query("SELECT * FROM users;").then(data => {
+    return data.rows;
+  })
+    .catch((error) => console.log(error));
+
+};
 
 const getUserById = id => {
-	return db.query("SELECT * FROM users WHERE id = $1;", [id]).then(data => {
-		return data.rows;
-	})
-}
+  return db.query("SELECT * FROM users WHERE id = $1;", [id]).then(data => {
+    return data.rows;
+  })
+    .catch((error) => console.log(error));
+};
 
-module.exports = {getAllUsers, getUserById}
+const getUserByEmail = (email) => {
+  return db
+    .query(`SELECT * FROM users WHERE email = $1;`, [email])
+    .then((user) => {
+      return user.rows;
+    })
+    .catch((error) => console.log(error));
+};
+
+const addUser = (username, email, password) => {
+  return db
+    .query(
+      `
+  INSERT INTO users (username, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *
+  `,
+      [username, email, password]
+    )
+    .then((user) => {
+      return user.rows[0];
+    })
+    .catch((error) => console.log(error));
+};
+
+
+
+module.exports = { getAllUsers, getUserById, getUserByEmail, addUser };
