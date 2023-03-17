9e1415a676445309f9ee1a9bcac48f5938f9e56e
Jonathan Phair <jmphair@gmail.com>
2023-03-17 10:58:27 -0400

    add: db seeds

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,4 +1,5 @@
 -- seeds/01_users.sql
 -- users seeds
+
 INSERT INTO users (id, username, email, password) VALUES (1, 'zelda', 'link@yahoo.com', 'hyrule');
 INSERT INTO users (id, username, email, password) VALUES (2, 'ihatewario', 'mario@mushroomkindom.jp', 'plumber79');
---
+++ server/db/seeds/04_food_items.sql
@@ -0,0 +1,16 @@
+-- seeds/04_food_items.sql
+-- food_items seeds
+
+INSERT INTO food_items (id, kitchen_inventories_id, grocery_list_id, name, quantity, storage_location) VALUES (1, 1, 1, 'chicken breast', 4, 'freezer');
+INSERT INTO food_items (id, kitchen_inventories_id, grocery_list_id, name, quantity, storage_location) VALUES (2, 1, 1, 'potato', 10, 'fridge');
+INSERT INTO food_items (id, kitchen_inventories_id, grocery_list_id, name, quantity, storage_location) VALUES (3, 1, 1, 'tomato', 'pantry');
+INSERT INTO food_items (id, kitchen_inventories_id, grocery_list_id, name, quantity, storage_location) VALUES (4, 1, 1, 'mustard', 1, 'fridge');
+INSERT INTO food_items (id, kitchen_inventories_id, grocery_list_id, name, quantity, storage_location) VALUES (5, 1, 1, 'flour', 'pantry');
+INSERT INTO food_items (id, kitchen_inventories_id, grocery_list_id, name, quantity, storage_location) VALUES (6, 1, 1, 'butter', 'fridge');
+
+INSERT INTO food_items (id, kitchen_inventories_id, grocery_list_id, name, quantity, storage_location) VALUES (7, 2, 1, 'steak', 3, 'freezer');
+INSERT INTO food_items (id, kitchen_inventories_id, grocery_list_id, name, quantity, storage_location) VALUES (8, 2, 1, 'sweet potato', 12, 'fridge');
+INSERT INTO food_items (id, kitchen_inventories_id, grocery_list_id, name, quantity, storage_location) VALUES (9, 2, 1, 'garlic', 'pantry');
+INSERT INTO food_items (id, kitchen_inventories_id, grocery_list_id, name, quantity, storage_location) VALUES (10, 2, 1, 'ketchup', 1, 'fridge');
+INSERT INTO food_items (id, kitchen_inventories_id, grocery_list_id, name, quantity, storage_location) VALUES (11, 2, 1, 'rice', 'pantry');
+INSERT INTO food_items (id, kitchen_inventories_id, grocery_list_id, name, quantity, storage_location) VALUES (12, 2, 1, 'olive oil', 'fridge');
