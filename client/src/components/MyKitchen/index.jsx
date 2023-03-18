28d263eefa796aa299fa328c9decaccef8a661e9
dorreb <ashley_ure@hotmail.com>
2023-03-18 15:32:01 -0400

    add: kitchen form

--- client/src/components/MyKitchen/index.jsx
+++ client/src/components/MyKitchen/index.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import { useState } from "react";
 
-import Form from './Form'
-import KitchenItemList from './KitchenItemList'
+import Form from "./Form";
+import KitchenItemList from "./KitchenItemList";
 
 const MyKitchen = () => {
+  const [showForm, setShowForm] = useState(false);
+
+  const handleAddItem = (event) => {
+    setShowForm(!showForm);
+  };
+
   return (
     <div>
       <KitchenItemList />
-      <Form />
+      {!showForm && <button onClick={handleAddItem}>Add Item</button>}
+      {showForm && (
+        <div className="kitchen-main">
+          <Form />
+          <button onClick={handleAddItem}>Cancel</button>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default MyKitchen;
