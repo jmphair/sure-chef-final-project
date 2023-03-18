c3b7e81d37ab4ea2afd39b9296569d45c5fb60ed
Jonathan Phair <jmphair@gmail.com>
2023-03-18 14:20:34 -0400

    add: state to dashboard

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,17 +1,28 @@
+import { useState } from 'react'
 import MyKitchen from './MyKitchen'
 import GroceryList from './GroceryList'
 import Recipe from './Recipe'
 import './Dashboard.css'
 
 const Dashboard = () => {
+  const [activeSection, setActiveSection] = useState("Dashboard");
+
+  function handleSectionClick(sectionName) {
+    setActiveSection(sectionName);
+  }
+
   return (
-    <main className='page'>
-      <p>Dashboard</p>
-      <MyKitchen />
-      <GroceryList />
-      <Recipe />
+    <main>
+      {activeSection === "Dashboard" && <div>
+      <button onClick={() => handleSectionClick("mykitchen")}>My Kitchen</button>
+      <button onClick={() => handleSectionClick("groceryList")}>My Grocery List</button>
+      <button onClick={() => handleSectionClick("recipe")}>My Recipes</button>
+      </div>}
+      {activeSection === "mykitchen" && <MyKitchen />}
+      {activeSection === "groceryList" && <GroceryList />}
+      {activeSection === "recipe" && <Recipe />}
     </main>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
