858e861c8ed5d3c67bcc518fd6a3c7467218aeea
Jonathan Phair <jmphair@gmail.com>
2023-03-19 17:00:16 -0400

    add: recipe generator component

---
+++ client/src/components/RecipeGenerator.jsx
@@ -0,0 +1,14 @@
+import { Container, Button } from 'react-bootstrap';
+
+
+function RecipeGenerator() {
+  
+  return (
+    <Container>
+      <Button variant="success">Generate Recipe</Button>
+    </Container>
+  );
+}
+
+export default RecipeGenerator;
+
