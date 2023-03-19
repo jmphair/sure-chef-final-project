279a50a0569e5b7e8df58082468df2ee2fe258ae
Jonathan Phair <jmphair@gmail.com>
2023-03-19 13:23:00 -0400

    add: placeholder examples and edit: imports

--- client/src/components/MyKitchen/KitchenItemList.jsx
+++ client/src/components/MyKitchen/KitchenItemList.jsx
@@ -1,11 +1,10 @@
-import React from "react";
 import KitchenItem from "./KitchenItem";
 import { Container, CardGroup } from "react-bootstrap";
 
 const KitchenItemList = () => {
   return (
     <Container>
-      <div>Kitchen item list:</div>
+      <div>Kitchen Item list:</div>
       <CardGroup>
         <KitchenItem />
         <KitchenItem />
