ad8aa84b491feb60e62c3658a57a5f87538b1510
dorreb <ashley_ure@hotmail.com>
2023-03-19 14:24:53 -0400

    add: show recipes on the screen

--- client/src/components/Recipe/index.jsx
+++ client/src/components/Recipe/index.jsx
@@ -1,10 +1,33 @@
 import RecipeItemList from "./RecipeItemList";
-import { Container } from 'react-bootstrap';
+import { Container } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Recipe = () => {
+  const [recipe, setRecipe] = useState({
+    name: "",
+    instructions: "",
+    servings: "",
+    prep_time: "",
+  });
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/recipes")
+      .then((response) => {
+        setRecipe(response.data.recipes[0]);
+        console.log(response.data.recipes[0]);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
-    <Container className='my-3'>
-      <RecipeItemList />
+    <Container className="my-3">
+      {/* <RecipeItemList /> */}
+      <h1>{recipe.name}</h1>
+      <p>{recipe.instructions}</p>
+      <p>{recipe.serving}</p>
+      <p>{recipe.prep_time}</p>
     </Container>
   );
 };
