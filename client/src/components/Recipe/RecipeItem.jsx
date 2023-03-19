7234ae6df2cbe44f5b5b07b88714e9b7dd70398a
dorreb <ashley_ure@hotmail.com>
2023-03-19 17:57:30 -0400

    add: recipe details to show up in the recipe container

--- client/src/components/Recipe/RecipeItem.jsx
+++ client/src/components/Recipe/RecipeItem.jsx
@@ -1,14 +1,18 @@
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button } from "react-bootstrap";
 
-const RecipeItem = () => {
+const RecipeItem = (props) => {
   return (
-    <Card className='my-3'>
+    <Card className="my-3">
+      <h1>{props.name}</h1>
+      <p>{props.instructions}</p>
+      <p>{props.serving}</p>
+      <p>{props.prep_time}</p>
       <Card.Body>
         <Card.Title>Recipe Item Name</Card.Title>
         <Card.Text>Ingredients: </Card.Text>
         <Card.Text>Directions: </Card.Text>
-        <Button variant="primary">Edit</Button>{' '}
-        <Button variant="danger">Delete</Button>{' '}
+        <Button variant="primary">Edit</Button>{" "}
+        <Button variant="danger">Delete</Button>{" "}
         <Button variant="success">Cook</Button>
       </Card.Body>
     </Card>
--- client/src/components/Recipe/RecipeItemList.jsx
+++ client/src/components/Recipe/RecipeItemList.jsx
@@ -1,13 +1,34 @@
 import RecipeItem from "./RecipeItem";
-import { Container, CardGroup } from 'react-bootstrap';
+import { Container, CardGroup } from "react-bootstrap";
+
+import { getRecipesForUsers } from "../../helpers/selectors";
+import useRecipeData from "../../hooks/useRecipeData";
 
 const RecipeItemList = () => {
+  console.log("DO I EXIST IN RecipeItemList COMPONENT");
+  // Use the useRecipeData hook to get the recipes and currentRecipe from state
+  const { recipes, currentRecipe } = useRecipeData();
+
+  // Get the recipes for a specific user (in this case, user ID 1) using the getRecipesForUsers function
+  const userRecipes =
+    recipes.length > 0 ? getRecipesForUsers({ recipes }, 1) : [];
+  // Map over the userRecipes array to create a Recipe component for each recipe
+  const recipeList = userRecipes.map((recipe) => {
+    return (
+      <RecipeItem
+        key={recipe.id}
+        name={recipe.name}
+        instructions={recipe.instructions}
+        servings={recipe.servings}
+        prepTime={recipe.prep_time}
+      />
+    );
+  });
   return (
-    <Container className='my-3'>
-      <h3 className='mb-3'>Recipe item list:</h3>
+    <Container className="my-3">
+      <h3 className="mb-3">Recipe item list:</h3>
       <CardGroup>
-        <RecipeItem />
-        <RecipeItem />
+        <>{recipeList}</>
       </CardGroup>
     </Container>
   );
