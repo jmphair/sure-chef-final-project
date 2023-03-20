28c8474534f33e5cbf9d6991dbc1da9353991daa
Jonathan Phair <jmphair@gmail.com>
2023-03-20 11:51:49 -0400

    edit: navbar fix

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,27 +1,34 @@
+import { useState } from 'react';
 import { Button, Container, Nav, Navbar, Offcanvas } from 'react-bootstrap';
 import Logout from './Logout';
 
 function Navigation(props) {
+  const [open, setOpen] = useState(true)
+
+  const handleClick = (link) => {
+    setOpen(false);
+    props.onClick(link);
+  }
 
   return (
-    <Navbar bg="light" expand={false} className="mb-3">
+    <Navbar expanded={open} bg="light" expand={false} className="mb-3" >
       <Container fluid>
         <Navbar.Brand href="#">SureChef</Navbar.Brand>
-        <Navbar.Toggle aria-controls="offcanvasNavbar" />
+        <Navbar.Toggle aria-controls="offcanvasNavbar" onClick={() => setOpen(true)}/>
         <Navbar.Offcanvas
           id="offcanvasNavbar"
           aria-labelledby="offcanvasNavbarLabel"
           placement="end"
         >
-          <Offcanvas.Header closeButton>
+          <Offcanvas.Header closeButton onClick={() => setOpen(false)}>
             <Offcanvas.Title id="offcanvasNavbarLabel">SureChef</Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
             <Nav className="justify-content-end flex-grow-1 pe-3">
-              <Button onClick={() => props.onClick("mykitchen")}>My Kitchen</Button>
-              <Button onClick={() => props.onClick("groceryList")}>Grocery List</Button>
-              <Button onClick={() => props.onClick("recipe")}>Recipes</Button>
-              <Button onClick={() => props.onClick("dashboard")}>Dashboard</Button>
+              <Button onClick={() => handleClick("mykitchen")}>My Kitchen</Button>
+              <Button onClick={() => handleClick("groceryList")}>Grocery List</Button>
+              <Button onClick={() => handleClick("recipe")}>Recipes</Button>
+              <Button onClick={() => handleClick("dashboard")}>Dashboard</Button>
               <Logout />
             </Nav>
           </Offcanvas.Body>
