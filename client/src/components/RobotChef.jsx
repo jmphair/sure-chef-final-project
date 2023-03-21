048e46eb75de4ca05a7f9422aeedf8cb76c996e8
Jonathan Phair <jmphair@gmail.com>
2023-03-21 18:19:22 -0400

    add: new hook

--- client/src/components/RobotChef.jsx
+++ client/src/components/RobotChef.jsx
@@ -2,52 +2,15 @@ import { useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import LightSwitchButton from './LightSwitchButton';
 import axios from 'axios';
+import useAiPrompt from '../hooks/useAiPrompt';
 
 function RobotChef() {
-  
-  const [prompt, updatePrompt] = useState(undefined);
-  const [loading, setLoading] = useState(false);
-  const [answer, setAnswer] = useState(undefined);
 
   const [light, setLight] = useState("off");
   const dark = (light === 'off') ? 'dark' : '';
   const switchLight = () => setLight((light === "on") ? "off" : "on");
 
-  useEffect(() => {
-    if (prompt != null && prompt.trim() === "") {
-      setAnswer(undefined);
-    }
-  }, [prompt]);
-
-  const sendPrompt = (event) => {
-    if (event.key !== "Enter") {
-      return Promise.resolve();
-    }
-  
-    // setLoading(true);
-  
-    const requestOptions = {
-      headers: { "Content-Type": "application/json" },
-    };
-  
-    return axios
-      .post("/api/openai/ask", { prompt }, requestOptions)
-      .then((res) => {
-        // if (!res) {
-        //   throw new Error("Something went wrong");
-        // }
-        
-        console.log(res);
-
-        const { message } = res.data;
-        setAnswer(message);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.error(err, "err");
-        setLoading(false);
-      });
-  };
+  const { loading, answer, sendPrompt, updatePrompt } = useAiPrompt();
 
   return (
     <Container>
