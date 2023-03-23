6a4972ed2922ba4dd065726ff3246fc3a91a8a99
Jonathan Phair <jmphair@gmail.com>
2023-03-23 09:15:48 -0400

    edit: recipe selector logic for saved recipes

--- client/src/helpers/selectors.jsx
+++ client/src/helpers/selectors.jsx
@@ -1,11 +1,11 @@
 /* function to get all the recipes that belong to a specific user */
-export function getRecipesForUsers(state, user_id) {
-  const recipes = state.recipes.filter((recipe) => recipe.user_id === user_id);
+export function getRecipesForUsers(state, user_id, saved) {
+  const recipes = state.recipes.filter((recipe) => recipe.user_id === user_id && recipe.saved === saved);
   return recipes;
 }
 
 export function getNewRecipeForUsers(state, user_id, saved) {
-  const newRecipe = state.recipes.filter((recipe) => recipe.user_id === user_id && recipe.saved === false);
+  const newRecipe = state.recipes.filter((recipe) => recipe.user_id === user_id && recipe.saved !== saved);
   return newRecipe;
 }
 
