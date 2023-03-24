dc66f3a35f4e74396f24afde3a1d13b6382b3fbd
Jonathan Phair <jmphair@gmail.com>
2023-03-24 17:36:57 -0400

    edit: comment out route

--- server/routes/openai-api.js
+++ server/routes/openai-api.js
@@ -61,30 +61,30 @@ router.post("/ask", (req, res) => {
   })
 });
 
-router.post("/potato", (req, res) => {
-  const prompt = recipePrompt(req.body.prompt, req.body.type);
+// router.post("/potato", (req, res) => {
+//   const prompt = recipePrompt(req.body.prompt, req.body.type);
 
-  if (prompt == null) {
-    throw new Error("Uh oh, no prompt was provided");
-  }
+//   if (prompt == null) {
+//     throw new Error("Uh oh, no prompt was provided");
+//   }
 
-  const responsePromise = openai.createCompletion({
-    model: "text-davinci-003",
-    prompt,
-    max_tokens: 1000,
-    temperature: 0,
-  });
+//   const responsePromise = openai.createCompletion({
+//     model: "text-davinci-003",
+//     prompt,
+//     max_tokens: 1000,
+//     temperature: 0,
+//   });
 
-  responsePromise.then((response) => {
-    const completion = response.data.choices[0].text;
-    return res.status(200).json({
-      success: true,
-      message: completion,
-    });
-  }).catch((error) => {
-    console.log(error.message);
-  });
-});
+//   responsePromise.then((response) => {
+//     const completion = response.data.choices[0].text;
+//     return res.status(200).json({
+//       success: true,
+//       message: completion,
+//     });
+//   }).catch((error) => {
+//     console.log(error.message);
+//   });
+// });
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
