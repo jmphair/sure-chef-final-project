71dddcd8fc8ad584069d24da832826ce60039d8a
dorreb <ashley_ure@hotmail.com>
2023-03-24 14:30:29 -0400

    style: add classNames and positionings

--- client/src/components/RecipeGenerator.jsx
+++ client/src/components/RecipeGenerator.jsx
@@ -1,32 +1,35 @@
-import {useState} from 'react'
+import { useState } from "react";
 import { Container, Button, Form } from "react-bootstrap";
+import "./RecipeGenerator.css";
 
 function RecipeGenerator(props) {
-  const [switchState, setSwitchState] = useState(false)
-  const [switchText, setSwitchText] = useState('"Only use the ingredients I select below!"')
-  const [recipeRestrictions, setRecipeRestrictions] = useState('strict')
+  const [switchState, setSwitchState] = useState(false);
+  const [switchText, setSwitchText] = useState(
+    '"Only use the ingredients I select below!"'
+  );
+  const [recipeRestrictions, setRecipeRestrictions] = useState("strict");
 
-  const handleChange=(e)=>{
-
+  const handleChange = (e) => {
     if (switchState === false) {
-      setSwitchState(true)
-      setSwitchText('"Tell me what I could add!"')
-      setRecipeRestrictions('flexible')
+      setSwitchState(true);
+      setSwitchText('"Tell me what I could add!"');
+      setRecipeRestrictions("flexible");
     }
 
     if (switchState === true) {
-      setSwitchState(false)
-      setSwitchText('"Only use the ingredients I select below!"')
-      setRecipeRestrictions('strict')
+      setSwitchState(false);
+      setSwitchText('"Only use the ingredients I select below!"');
+      setRecipeRestrictions("strict");
     }
-  }
-     
+  };
 
   return (
     <Container>
-      <>Ok {props.user.name}, what's the plan?</>
-      <Form>
-        <Form.Check 
+      <div className="message">
+        Ok {props.user.name}, toggle the button below to get started!
+      </div>
+      <Form className="toggle">
+        <Form.Check
           type="switch"
           id="custom-switch"
           defaultChecked={switchState}
@@ -34,9 +37,14 @@ function RecipeGenerator(props) {
         />
         {switchText}
       </Form>
-      <Button onClick={() => props.generateRecipe(recipeRestrictions)} variant="outline-dark">
-        Create Recipe
-      </Button>
+      <div className="create-button">
+        <Button
+          onClick={() => props.generateRecipe(recipeRestrictions)}
+          variant="outline-dark"
+        >
+          Create Recipe
+        </Button>
+      </div>
     </Container>
   );
 }
