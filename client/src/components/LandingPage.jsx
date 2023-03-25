da5e772c16ba9d657917dee1769dee4d6dd90a86
Jonathan Phair <jmphair@gmail.com>
2023-03-25 15:04:46 -0400

    add: landing page photos

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import { Container, Carousel, Image } from "react-bootstrap";
 import Userfront from "@userfront/react";
+import robot from "../assets/RobotChef(big).gif";
+import pasta from "../assets/pasta.jpeg"
+import robotHands from "../assets/robothands.jpg"
+
 
 Userfront.init("wbmy6rvb");
 
@@ -16,32 +20,26 @@ const LandingPage = () => {
       <Carousel activeIndex={index} onSelect={handleSelect}>
         <Carousel.Item>
           <Image
-            fluid
             rounded
             className="d-block w-100"
-            src="https://api.time.com/wp-content/uploads/2015/04/moley-robotics-automated-kitchen_lr.jpg?quality=85&w=800"
-            alt="First slide"
-            style={{ width: "200px", height: "300px" }}
+            src={robot}
+            alt="Robot Kitchen Video"
           />
         </Carousel.Item>
         <Carousel.Item>
           <Image 
-            fluid
             rounded
             className="d-block w-100"
-            src="https://cdn3.tim-europe.com/websites/pcne/typo3temp/assets/_processed_/5/4/csm_Pic_1_robot_hand_apple_bboard_misc_aa1a1407de.jpg"
-            alt="Second slide"
-            style={{ width: "200px", height: "300px" }}
+            src={pasta}
+            alt="Plated Pasta Photo"
           />
         </Carousel.Item>
         <Carousel.Item>
           <Image 
-            fluid
             rounded
             className="d-block w-100"
-            src="https://alorestaurant.com/wp-content/uploads/2019/12/Alo-Food-Shot-10-Hi-Res.jpg"
-            alt="Third slide"
-            style={{ width: "200px", height: "300px" }}
+            src={robotHands}
+            alt="Robot Cooking Photo"
           />
         </Carousel.Item>
       </Carousel>
