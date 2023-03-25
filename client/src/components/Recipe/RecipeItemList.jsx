b2ff818f784765dcb6b3a8e21581e7a6d37f5d86
dorreb <ashley_ure@hotmail.com>
2023-03-25 01:04:16 -0400

    style: add accordian to recipes page and order them

--- client/src/components/Recipe/RecipeItemList.jsx
+++ client/src/components/Recipe/RecipeItemList.jsx
@@ -1,6 +1,5 @@
 import RecipeItem from "./RecipeItem";
-import { Container, CardGroup } from "react-bootstrap";
-
+import { Container, CardGroup, Card, Accordion } from "react-bootstrap";
 import { getRecipesForUsers } from "../../helpers/selectors";
 import useRecipeData from "../../hooks/useRecipeData";
 
@@ -25,8 +24,10 @@ const RecipeItemList = (props) => {
   const userRecipes =
     recipes.length > 0 ? getRecipesForUsers({ recipes }, props.user.id) : [];
 
+  const userRecipesReversed = userRecipes.slice().reverse();
+
   // Map over the userRecipes array to create a Recipe component for each recipe
-  const recipeList = userRecipes.map((recipe) => {
+  const recipeList = userRecipesReversed.map((recipe) => {
     let ingredients = "";
     recipe.ingredients.forEach((ingredient) => {
       ingredients += Object.keys(ingredient)[0] + " x ";
@@ -43,35 +44,40 @@ const RecipeItemList = (props) => {
     instructions = instructions.slice(0, -1);
 
     return (
-      <RecipeItem
-        key={recipe.id}
-        id={recipe.id}
-        name={recipe.name}
-        instructions={instructions}
-        ingredients={ingredients}
-        servings={recipe.servings}
-        prepTime={recipe.prep_time}
-        cookTime={recipe.cook_time}
-        totalTime={recipe.total_time}
-        note={recipe.note}
-        saved={recipe.saved}
-        showOnEdit={showOnEdit}
-        onDelete={handleDelete}
-        handleSectionClick={props.handleSectionClick}
-      />
+      <Container style={{ margin: 0, padding: 0 }}>
+        <Accordion className="custom-accordion" defaultActiveKey={recipe.id}>
+          <Accordion.Header>{recipe.name}</Accordion.Header>
+          <Accordion.Body>
+            <RecipeItem
+              key={recipe.id}
+              id={recipe.id}
+              name={recipe.name}
+              instructions={instructions}
+              ingredients={ingredients}
+              servings={recipe.servings}
+              prepTime={recipe.prep_time}
+              cookTime={recipe.cook_time}
+              totalTime={recipe.total_time}
+              note={recipe.note}
+              saved={recipe.saved}
+              showOnEdit={showOnEdit}
+              onDelete={handleDelete}
+              handleSectionClick={props.handleSectionClick}
+            />
+          </Accordion.Body>
+        </Accordion>
+      </Container>
     );
   });
 
-  const count = userRecipes.length
-  console.log(count)
+  const count = userRecipes.length;
 
   return (
     <Container className="my-3">
       <h3 className="heading">My Cookbook</h3>
       <h6 className="heading">Total Recipes: {count}</h6>
-      <CardGroup>
-        <>{recipeList}</>
-      </CardGroup>
+
+      <Accordion>{recipeList}</Accordion>
     </Container>
   );
 };
