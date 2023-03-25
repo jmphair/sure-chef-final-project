21ae429e7c189c1794e0d8b6cd892ed92944f11c
dorreb <ashley_ure@hotmail.com>
2023-03-25 14:26:44 -0400

    update: schema to include timestamp

--- server/db/schema/05_create_recipes.sql
+++ server/db/schema/05_create_recipes.sql
@@ -6,12 +6,13 @@ CREATE TABLE recipes (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   ingredients jsonb NOT NULL,
-  instructions TEXT[] NOT NULL,
+  instructions TEXT [] NOT NULL,
   servings VARCHAR(255),
   prep_time VARCHAR(255),
   cook_time VARCHAR(255),
   total_time VARCHAR(255),
   saved BOOLEAN DEFAULT false,
   note TEXT,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  created_at TIMESTAMP DEFAULT NOW()
 );
