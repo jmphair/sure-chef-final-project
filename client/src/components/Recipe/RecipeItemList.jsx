acf1aa29d3fbca6e2bbb5641b21ccf7c1f3280d9
dorreb <ashley_ure@hotmail.com>
2023-03-25 10:44:54 -0400

    add: search functionality in recipes

--- client/src/components/Recipe/RecipeItemList.jsx
+++ client/src/components/Recipe/RecipeItemList.jsx
@@ -1,4 +1,5 @@
 import RecipeItem from "./RecipeItem";
+import { useState } from "react";
 import { Container, CardGroup, Card, Accordion } from "react-bootstrap";
 import { getRecipesForUsers } from "../../helpers/selectors";
 import useRecipeData from "../../hooks/useRecipeData";
@@ -6,6 +7,7 @@ import useRecipeData from "../../hooks/useRecipeData";
 const RecipeItemList = (props) => {
   // Use the useRecipeData hook to get the recipes and currentRecipe from state
   const { recipes, setRecipes } = useRecipeData();
+  const [searchQuery, setSearchQuery] = useState("");
 
   /* function used in EditForm component to update the state after an item is edited  */
   const showOnEdit = (updatedNote) => {
@@ -20,14 +22,28 @@ const RecipeItemList = (props) => {
     setRecipes((prev) => prev.filter((recipe) => recipe.id !== id));
   };
 
+  const handleSearchInputChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
   // Get the recipes for a specific user (in this case, user ID 1) using the getRecipesForUsers function
   const userRecipes =
     recipes.length > 0 ? getRecipesForUsers({ recipes }, props.user.id) : [];
 
-  const userRecipesReversed = userRecipes.slice().reverse();
+  // sort the recipes by the created_at time
+  let sortedRecipes = userRecipes
+    .slice()
+    .sort((a, b) => b.created_at.localeCompare(a.created_at));
+
+  // check for recipe names that include the searchQuery string
+  if (searchQuery !== "") {
+    sortedRecipes = sortedRecipes.filter((recipe) =>
+      recipe.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  }
 
   // Map over the userRecipes array to create a Recipe component for each recipe
-  const recipeList = userRecipesReversed.map((recipe) => {
+  const recipeList = sortedRecipes.map((recipe) => {
     let ingredients = "";
     recipe.ingredients.forEach((ingredient) => {
       ingredients += Object.keys(ingredient)[0] + " x ";
@@ -76,7 +92,18 @@ const RecipeItemList = (props) => {
     <Container className="my-3">
       <h3 className="heading">My Cookbook</h3>
       <h6 className="heading">Total Recipes: {count}</h6>
-
+      <div className="mb-3">
+        <label htmlFor="searchInput" className="form-label">
+          Search for a recipe
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="searchInput"
+          value={searchQuery}
+          onChange={handleSearchInputChange}
+        />
+      </div>
       <Accordion>{recipeList}</Accordion>
     </Container>
   );
