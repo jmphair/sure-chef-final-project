cc545c6b8451b1926678cc2333baf3ae1de1dbfc
Jonathan Phair <jmphair@gmail.com>
2023-03-25 21:18:30 -0400

    add: dark draft

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, createContext } from "react";
 import { Container, Button, CardGroup, Card, Modal } from "react-bootstrap";
 
 import Navigation from "./Navigation";
@@ -10,10 +10,14 @@ import GoToTop from "./GoToTop";
 import "./Dashboard.css";
 import Potato from "./Potato";
 import { welcomeMessage } from "../helpers/welcomeMessage";
+import ReactSwitch from "react-switch";
+
+export const ThemeContext = createContext(null);
 
 const Dashboard = (props) => {
   const [activeSection, setActiveSection] = useState("dashboard");
   const [show, setShow] = useState(false);
+  const [theme, setTheme] = useState("dark")
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -22,78 +26,88 @@ const Dashboard = (props) => {
     setActiveSection(sectionName);
   }
 
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"))
+  };
+
   return (
-    <main>
-      <Navigation user={props.user} onClick={handleSectionClick} />
-      {activeSection === "dashboard" && (
-        <Container className="my-3">
-          {welcomeMessage(props.user.name)}
-          <Card>
-            <Button
-              onClick={() => handleSectionClick("mykitchen")}
-              variant="outline-dark"
-            >
-              My Kitchen
-            </Button>
-          </Card>
-          <Card>
-            <Button
-              onClick={() => handleSectionClick("groceryList")}
-              variant="outline-dark"
-            >
-              My Grocery List
-            </Button>
-          </Card>
-          <Card>
-            <Button
-              onClick={() => handleSectionClick("recipe")}
-              variant="outline-dark"
-            >
-              My Recipes
-            </Button>
-          </Card>
-          
-          <div className="veg-footer1" style={{ textAlign: "center" }}>
-          <hr className="solid"/>
-            <>🥬🧅🌶🍅</>
-            <span
-              className="potato"
-              style={{ cursor: "pointer" }}
-              variant="primary"
-              onClick={handleShow}
-            >
-              🥔
-            </span>
-            <>🥕🥦🌽🍠</>
-          </div>
-          <div className="veg-footer2" style={{ textAlign: "center" }}>
-            Special Thanks To Our Instructors At LHL
-          </div>
-          <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>
-                You have clicked the potato of destiny, what may I reveal to
-                you?
-              </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <Potato />
-            </Modal.Body>
-          </Modal>
-        </Container>
-      )}
-      {activeSection === "mykitchen" && (
-        <MyKitchen user={props.user} handleSectionClick={handleSectionClick} />
-      )}
-      {activeSection === "groceryList" && <GroceryList user={props.user} />}
-      {activeSection === "recipe" && (
-        <Recipe user={props.user} handleSectionClick={handleSectionClick} />
-      )}
-      {activeSection === "saverecipe" && (
-        <SaveRecipe user={props.user} handleSectionClick={handleSectionClick} />
-      )}
-      <GoToTop />
-    </main>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <main className="App" id={theme}>
+        <Navigation user={props.user} onClick={handleSectionClick} />
+        {activeSection === "dashboard" && (
+          <Container className="my-3">
+            {welcomeMessage(props.user.name)}
+            <Card>
+              <Button
+                onClick={() => handleSectionClick("mykitchen")}
+                variant="outline-dark"
+              >
+                My Kitchen
+              </Button>
+            </Card>
+            <Card>
+              <Button
+                onClick={() => handleSectionClick("groceryList")}
+                variant="outline-dark"
+              >
+                My Grocery List
+              </Button>
+            </Card>
+            <Card>
+              <Button
+                onClick={() => handleSectionClick("recipe")}
+                variant="outline-dark"
+              >
+                My Recipes
+              </Button>
+            </Card>
+            
+            <div className="veg-footer1" style={{ textAlign: "center" }}>
+              <div className="switch">
+                <label> {theme === "light" ? "Light Mode" : "Dark Mode"} </label>
+                <ReactSwitch onChange={toggleTheme} checked={theme === "dark"}/>
+              </div>
+            <hr className="solid"/>
+              <>🥬🧅🌶🍅</>
+              <span
+                className="potato"
+                style={{ cursor: "pointer" }}
+                variant="primary"
+                onClick={handleShow}
+              >
+                🥔
+              </span>
+              <>🥕🥦🌽🍠</>
+            </div>
+            <div className="veg-footer2" style={{ textAlign: "center" }}>
+              Special Thanks To Our Instructors At LHL
+            </div>
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>
+                  You have clicked the potato of destiny, what may I reveal to
+                  you?
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Potato />
+              </Modal.Body>
+            </Modal>
+          </Container>
+        )}
+        {activeSection === "mykitchen" && (
+          <MyKitchen user={props.user} handleSectionClick={handleSectionClick} />
+        )}
+        {activeSection === "groceryList" && <GroceryList user={props.user} />}
+        {activeSection === "recipe" && (
+          <Recipe user={props.user} handleSectionClick={handleSectionClick} />
+        )}
+        {activeSection === "saverecipe" && (
+          <SaveRecipe user={props.user} handleSectionClick={handleSectionClick} />
+        )}
+        <GoToTop />
+      </main>
+    </ThemeContext.Provider>
   );
 };
 
